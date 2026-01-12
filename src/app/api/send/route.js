0fd90b2c591f9bce4eb5284ccaf76b06adaf3c84
Mohammad Tarique <mohammadtarique661998@gmail.com>
2026-01-12 22:21:54 +0530

    Latest Update - 11/01/2026:-13

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -78,7 +78,7 @@ export async function POST(req) {
   }
 }
 
-// import { Resend } from "resend"; const resend = new Resend(process.env.RESEND_API_KEY); export async function POST(req) { try { const { email, subject, message } = await req.json(); if (!email || !subject || !message) { return new Response( JSON.stringify({ error: "Missing fields" }), { status: 400 } ); } await resend.emails.send({ from: "Portfolio Contact Mail<onboarding@resend.dev>", to: [process.env.CONTACT_EMAIL], reply_to: email, subject: subject, html: <h2>New Message from Portfolio Website</h2> <p><strong>From:</strong> ${email}</p> <p><strong>Message:</strong></p> <p>${message}</p> , }); return new Response( JSON.stringify({ success: true }), { status: 200 } ); } catch (error) { console.error(error); return new Response( JSON.stringify({ error: "Failed to send email" }), { status: 500 } ); } }
+// import { Resend } from "resend";
 // const resend = new Resend(process.env.RESEND_API_KEY);
 
 // export async function POST(req) {
